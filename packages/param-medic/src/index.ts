4607ba2885269e6deb09c8241bf00e921f1e6674
bpetermann <benjamin.petermann@gmx.at>
2025-02-22 09:05:14 +0100

    chore: export use param context

--- packages/param-medic/src/index.ts
+++ packages/param-medic/src/index.ts
@@ -1,3 +1,4 @@
+export { useParamContext } from './lib/context/actions';
 export { ParamContextProvider } from './lib/context/provider';
 export { useParams } from './lib/useParams';
 export { buildUrlWithParams } from './lib/utils';
